4cfb1b37aa152ede728a3c22a6069bddb42d7a08
Tamar-Amar <amtamar747@gmail.com>
2025-01-05 22:21:16 +0200

    apiRoute

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 export async function login(operatorName) {
   const response = await fetch(`${API_URL}/login`, {
